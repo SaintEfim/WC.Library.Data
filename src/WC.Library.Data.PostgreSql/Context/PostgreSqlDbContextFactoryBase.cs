2cb75377d131eaced0a86b7caa03e5876e5ec907
SaintEfim <saintefim2@gmail.com>
2024-04-19 10:08:32 +0300

    added new feature

---
+++ src/WC.Library.Data.PostgreSql/Context/PostgreSqlDbContextFactoryBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace WC.Library.Data.PostgreSql.Context;
+
+public class PostgreSqlDbContextFactoryBase<TDbContext> where TDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    protected PostgreSqlDbContextFactoryBase(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected virtual string ConnectionString => null!;
+
+    public TDbContext CreateDbContext()
+    {
+        var connectionString = _configuration.GetConnectionString(ConnectionString);
+
+        var optionsBuilder = new DbContextOptionsBuilder<TDbContext>();
+        optionsBuilder.UseNpgsql(connectionString, options => { options.CommandTimeout(30); });
+        optionsBuilder.EnableDetailedErrors();
+        optionsBuilder.EnableSensitiveDataLogging();
+
+        return (TDbContext)Activator.CreateInstance(typeof(TDbContext), optionsBuilder.Options)!;
+    }
+}
