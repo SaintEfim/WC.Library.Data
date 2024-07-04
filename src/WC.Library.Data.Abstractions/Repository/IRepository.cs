a9b64e69f6a93fb8d86d280e69ad123604347940
SaintEfim <saintefim2@gmail.com>
2024-07-04 18:25:22 +0300

    extending the capabilities of the get method

--- src/WC.Library.Data.Abstractions/Repository/IRepository.cs
+++ src/WC.Library.Data.Abstractions/Repository/IRepository.cs
@@ -4,7 +4,7 @@ namespace WC.Library.Data.Repository;
 
 public interface IRepository<TEntity> where TEntity : class, IEntity
 {
-    Task<IEnumerable<TEntity>> Get(CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> Get(bool withIncludes = false, CancellationToken cancellationToken = default);
     Task<TEntity> Create(TEntity entity, CancellationToken cancellationToken = default);
     Task<TEntity?> GetOneById(Guid id, CancellationToken cancellationToken = default);
     Task<TEntity> Update(TEntity entity, CancellationToken cancellationToken = default);
